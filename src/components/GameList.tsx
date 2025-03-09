3a42685af0590db3e20978274b1bfe9e9a652bb8
heikern <heikern@hotmail.com>
2025-03-09 17:13:06 +0800

    removed useState

--- src/components/GameList.tsx
+++ src/components/GameList.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { fetchAdventures } from '../store/firebaseSlice';
 import { setSelectedGameId } from '../store/gameSlice';
